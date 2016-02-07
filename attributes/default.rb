04172b2971098cdc7a4e68f6f7a48ea9a1c48436
skytap <skytap@host-2.skytap.example>
2016-02-07 16:43:22 +0000

    Removing username

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,3 +1,2 @@
-default['skytap']['username'] = 'bcoleman@skytap.com'
-default['skytap']['password'] = 'b59f7134ba822a1ca64fd86d203f1c8627cdcef0'
-default['skytap']['env_id'] = ''
+default['skytap']['username'] = 'changeme'
+default['skytap']['password'] = 'changeme'
